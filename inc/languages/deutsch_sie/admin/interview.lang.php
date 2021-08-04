c54b5e549b100cbc802938b680fd53edd85d158a
Julia Roloff <72280520+ItsSparksFly@users.noreply.github.com>
2021-08-04 13:17:55 +0200

    Create interview.lang.php

---
+++ inc/languages/deutsch_sie/admin/interview.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+$l['interview_title'] = "Interview";
+$l['interview_desc'] = "Lässt Admins im AdminCP Interviewfragen festlegen, die User anschließend in ihrem UserCP beantworten können.";
+$l['interview_permission'] = "Kann Interviewfragen hinzufügen?";
+$l['interview_add'] = "Fragen hinzufügen";
+$l['interview_edit'] = "Fragen bearbeiten";
+$l['interview_manage'] = "Interviewfragen verwalten";
+$l['interview_add_desc'] = "User können diese Fragen anschließend im UserCP beantworten.";
+$l['interview_question'] = "Frage";
+$l['interview_question_desc'] = "Wie lautet die Frage, die User mit ihren Charakteren beantworten müssen?";
+$l['interview_added'] = "Frage wurde hinzugefügt.";
+$l['interview_deleted'] = "Frage wurde erfolgreich gelöscht.";
+$l['interview_invalid'] = "Ungültige Frage ausgewählt.";
+$l['interview_number'] = "Anzahl Antworten im Profil";
+$l['interview_number_desc'] = "Wie viele Antworten sollen im Profil angezeigt werden? Bei leer gelassenem Feld werden alle Antworten ausgegeben. Sollen weniger Antworten angezeigt werden als vorhanden sind, werden die Antworten zufällig ausgewürfelt.";
+$l['interview_options'] = "Einstellungen für das Interview-Plugin";
+?>
