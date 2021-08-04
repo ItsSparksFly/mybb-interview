58e6a040c6a78426decba0179f8c6a54186b9847
Julia Roloff <72280520+ItsSparksFly@users.noreply.github.com>
2021-08-04 13:18:08 +0200

    Create interview.lang.php

---
+++ inc/languages/deutsch_sie/interview.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$l['interview'] = "Interview";
+$l['interview_submit'] = "Antworten speichern";
+$l['interview_answercount'] = "Charaktere haben diese Frage bisher beantwortet!";
+$l['interview_online'] = "Beantwortet Interviewfragen";
+?>
